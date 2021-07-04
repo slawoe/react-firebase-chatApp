adb3020d55db3e1c296bc93093729ad4e0fb5a7e
slawoe <info@slawomirernst.de>
2021-07-04 13:58:57 +0200

    Add useEffect for getting messages (snapshot) in Chat.js and mapping them

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase";
 import SignOut from "./SignOut";
+
 function Chat() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    db.collection("messages")
+      .orderBy("createdAt")
+      .limit(50)
+      .onSnapshot((snapshot) => {
+        setMessages(snapshot.docs.map((doc) => doc.data()));
+      });
+  }, []);
+
   return (
     <div>
       <SignOut />
+      {messages.map(({ id, text, photoURL }) => (
+        <div key={id}>
+          <img src={photoURL} alt="Profile portrait" />
+          <p>{text}</p>
+        </div>
+      ))}
     </div>
   );
 }
