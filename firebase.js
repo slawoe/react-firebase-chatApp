f37e4bf466f5fb0669445a46cbc227435c789309
slawoe <info@slawomirernst.de>
2021-07-04 11:49:50 +0200

    Add firebase setup

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "APIKEY",
+  authDomain: "react-firebase-chatapp-5b9e0.firebaseapp.com",
+  projectId: "react-firebase-chatapp-5b9e0",
+  storageBucket: "react-firebase-chatapp-5b9e0.appspot.com",
+  messagingSenderId: "544072095891",
+  appId: "1:544072095891:web:38bdc866fa75a86b4b9566",
+  measurementId: "G-F4MS32RJ0S",
+});
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export { db, auth };
